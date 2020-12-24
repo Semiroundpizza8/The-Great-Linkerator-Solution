d04186fa72e0031c72ecf5afaeb630fdc9100644
Andrew <oleandy1221@gmail.com>
2020-12-23 19:36:34 -0600

    added useEffect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import fetchAPI from './api';
+
+
 
 const App = () => {
-  return <h1>YEET MAN</h1>;
+  const [linkList, setLinkList] = useState([]);
+  const [search, setSearch] = useState('');
+
+useEffect(async () => {
+    fetchAPI('http://localhost:3001/api/links')
+      .then((resp) => {
+        setLinkList(resp);
+      })
+      .catch(console.error);
+}, [])
+
+function filteredLinks() {
+  return linkList.filter((_link) => {
+    return _link.link.includes(search.toLowerCase());
+  });
+}
+
+
+  return <h1>The Great Linkerator</h1>;
 };
 
 ReactDOM.render(
