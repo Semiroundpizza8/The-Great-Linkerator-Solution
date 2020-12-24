034916d2916d53d5f11b152140d56d8d0389a73b
deCha <decha-sanson-1978@deChas-MacBook-Pro.local>
2020-12-23 18:38:30 -0600

    Adding api files, added methods in data_layer for updating, getting by Id, and creating initial links

---
+++ api/index.js
@@ -0,0 +1,69 @@
+const {client, getAllLinks, createLink, updateLink, getLinkById} = require('../data_layer');
+
+const apiRouter = require("express").Router();
+
+apiRouter.get('/', (req, res, next) => {
+    res.send({message: 'Welcome to The Great Linkerator'})
+})
+
+apiRouter.get("/links", async (req, res, next) => {
+    try {
+      console.log("Requesting all links")
+      const allLinks = await getAllLinks();
+  
+      res.send(allLinks)
+      next();
+  
+      console.log("Finished requesting all links");
+    } catch(error) {
+      console.log("Error requesting links");
+      next(error);
+    }
+  });
+
+  apiRouter.post("/links", async (req, res, next) => {
+    try {
+      const newLink = await createLink(req.body);
+  
+      res.send(newLink);
+      next();
+    } catch(error) {
+      next(error);
+    }
+  })
+
+  apiRouter.patch("/links/:id", async (req, res, next) => {
+    const { linkId } = req.params; 
+    const { link, comment, clickCount} = req.body;
+
+    const updateFields = {};
+
+    if(link) {
+        updateFields.linkName = link;
+    }
+
+    if(comment) {
+        updateFields.comment = comment;
+    }
+
+    if(clickCount) {
+        updateFields.clickCount = clickCount;
+    }
+
+    try {
+        const originalLink = getLinkById(linkId);
+
+        if(originalLink.id === linkId) {
+            const updatedLink = await updateLink(linkId, updateFields);
+            res.send({link: updatedLink })
+        } else {
+            next({
+                message: "Error updating link"
+            })
+        }
+    } catch(error) {
+        throw error;
+    }
+  })
+
+  module.exports = apiRouter;
