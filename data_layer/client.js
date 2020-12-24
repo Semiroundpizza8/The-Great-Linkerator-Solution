580c2a84eebc537b40e5d219c2b6391654a4fcab
Kris Dandridge <krisdandridge@kriss-mbp.lan>
2020-12-23 20:00:54 -0600

    new repo push

--- data_layer/client.js
+++ data_layer/client.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/linkerator";
+  process.env.DATABASE_URL || "postgres://localhost:5432/linkerator-dev";
 
 const client = new Client(DATABASE_URL);
 
