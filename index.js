a2f45f3b8bdfb98014fd32ff9c00c744d7de1922
deCha <decha-sanson-1978@deChas-MacBook-Pro.local>
2020-12-30 17:05:37 -0600

    bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 require('dotenv').config();
 
 const {client, getAllLinks, createLink, createInitialLink} = require('./data_layer');
@@ -20,7 +18,7 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // make a route for each front end page
 // each time you add a front end route using react router, it needs to be added to this array
-["create", "searchBar", ""].forEach((route) => {  app.get(`/${route}`, (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html"));  });});
+//["createLink", "searchBar", ""].forEach((route) => {  app.get(`/${route}`, (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html"));  });});
 
 const apiRouter = require('./api');
 app.use('/api', apiRouter);
--- src/index.js
+++ src/index.js
@@ -9,36 +9,38 @@ const App = () => {
   const [linkList, setLinkList] = useState([]);
   const [search, setSearch] = useState('');
   console.log('hello there!')
-// useEffect(async () => {
-//     fetchAPI('http://localhost:3001/api/links')
-//       .then((resp) => {
-//         setLinkList(resp);
-//       })
-//       .catch(console.error);
-// }, [])
 
-function filteredLinks() {
-  return linkList.filter((_link) => {
-    return _link.link.includes(search.toLowerCase());
-  });
-}
+  function addNewLink(newLink) {
+    setLinkList([...linkList, newLink]);
+  }
+  
+  let history = useHistory();
+
+  // useEffect(async () => {
+  //   fetchAPI('http://localhost:3001/api/links')
+  //     .then((resp) => {
+  //       setLinkList(resp);
+  //     })
+  //     .catch(console.error);
+  // }, [])
+
+  function filteredLinks() {
+    return linkList.filter((_link) => {
+      return _link.link.includes(search.toLowerCase());
+    });
+  }
+  
   return <>
   <h1>The Great Linkerator</h1>
   <Switch>
     <Route path="/searchBar" render={()=> <SearchBar />} />
-    {/* <Route path="/createLink" render={()=><CreateLinkForm linkList={linkList} setLinkList={setLinkList} addNewLink={addNewLink} history={history}/>}/> */}
+    <Route path="/createLink" render={()=><CreateLinkForm linkList={linkList} setLinkList={setLinkList} addNewLink={addNewLink} history={history}/>}/>
 
-    {/* <Redirect from="*" to="/"  /> */}
+    <Redirect from="*" to="/"  />
   </Switch>
 
   </>
 };
-
-// function addNewLink(newLink) {
-//   setLinkList([...linkList, newLink]);
-// }
-
-//let history = useHistory();
 
 ReactDOM.render(
   <Router>
